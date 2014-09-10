581e2005fe5f3755f9a37969230f129fdee17c57
batedurgonnadie <batedurgonnadie@yahoo.com>
2014-09-10 02:26:19 -0700

    remove_www from glacials/splits-io

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   include TwitchHelper
+
+  before_action :remove_www
+
+  def remove_www
+    redirect_to(subdomain: nil) if request.subdomain == 'www'
+  end
+  #"borrowed" from glacials/splits-io
 end
