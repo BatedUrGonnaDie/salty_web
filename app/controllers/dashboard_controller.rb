2fc39ce4fe642c30eb2ec27b77c2b768d25d4837
batedurgonnadie <batedurgonnadie@yahoo.com>
2014-11-06 14:53:56 -0800

    test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,12 +6,12 @@ class DashboardController < ApplicationController
 
   def from_twitch
     oauth = HTTParty.post("https://api.twitch.tv/kraken/oauth2/token", query: full_twitch_info)['access_token']
-    @@info = oauth
+    @info = oauth
     redirect_to dashboard_path
   end
 
   def dashboard
-    @info = @@info
+    
   end
 
   private
