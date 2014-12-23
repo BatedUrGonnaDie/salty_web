875a50aee0ecc8ea69f1c4e68bf1480763c0ae48
batedurgonnadie <batedurgonnadie@yahoo.com>
2014-12-23 05:17:11 -0800

    Fix formatting

--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -1,15 +1,19 @@
 class Api::QuotesController < Api::ApplicationController
 
   def text_show
-    user = User.find_by(twitch_name: params[:user])
+    user = User.find_by(twitch_name: params[:user]) || false
     if user
       u_quotes = user.quotes
+      if u_quotes.length == 0
+        render status: 400, json: {status: 400, error: "No quotes for this user."}
+        return
+      end
       r_quote_array = []
       num_used = []
       limit = params[:limit] || 1
       limit.times {num_used.push(rand(u_quotes.length))}
       num_used.each do |x| 
-        r_quote_array.push(u_quotes[x])
+        r_quote_array.push(u_quotes[x].text)
       end
       render status: 200, json: {status: 200, text: r_quote_array}
     else
