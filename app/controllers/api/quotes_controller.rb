8ae2aeeaece88f3bfee3666def8d2bb53a118a6c
glacials <qhiiyr@gmail.com>
2014-12-25 01:07:17 -0800

    Refactor Api::QuotesController and routes for it

--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -1,25 +1,33 @@
 class Api::QuotesController < Api::ApplicationController
+  before_action :set_user
+  before_action :check_for_quotes
 
-  def text_show
-    user = User.find_by(twitch_name: params[:user]) || false
-    if user
-      u_quotes = user.quotes
-      if u_quotes.length == 0
-        render status: 400, json: {status: 400, error: "No quotes for this user."}
-        return
-      end
-      r_quote_array = []
-      num_used = []
-      limit = params[:limit] || 1
-      limit.times {num_used.push(rand(u_quotes.length))}
-      num_used.each do |x| 
-        r_quote_array.push(u_quotes[x].text)
-      end
-      render status: 200, json: {status: 200, text: r_quote_array}
-    else
+  def index
+    render status: 200, json: {
+      status: 200,
+      quotes: @user.quotes.sample(limit).map { |quote| {text: quote.text} }
+    }
+  end
+
+  private
+
+  def set_user
+    unless @user = User.find_by(twitch_name: params[:user_id])
       render status: 400, json: {status: 400, error: "Invalid user."}
     end
   end
 
+  def check_for_quotes
+    if @user.quotes.empty?
+      render status: 400, json: {status: 400, error: "No quotes for this user."}
+    end
+  end
 
+  def limit
+    if params[:limit].present?
+      params[:limit].to_i < 0 ? 0 : params[:limit].to_i
+    else
+      1
+    end
+  end
 end
