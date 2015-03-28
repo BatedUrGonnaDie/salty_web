51002affc8707a1c77365952b155a0d47af2cd68
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-03-28 00:15:07 -0700

    Make quotes page not suck as much

--- app/assets/javascripts/textutil_approve.js
+++ app/assets/javascripts/textutil_approve.js
@@ -4,6 +4,9 @@ $(function() {
             url: "/api/user/" + util["user"] + "/" + util["type"] + "/" + util["id"],
             type: "PUT",
             data: "&reviewed=1",
+            success: function() {
+                $("#approve-" + util["id"]).replaceWith("<div class='approve-check'>\u2713</div>")
+            },
             error: function(response) {
                 console.log("Creatoin of util failed with response: " + JSON.stringify(response));
             }
--- app/assets/javascripts/textutil_delete.js
+++ app/assets/javascripts/textutil_delete.js
@@ -3,6 +3,9 @@ $(function() {
         $.ajax({
             url: "/api/user/" + util["user"] + "/" + util["type"] + "/" + util["id"],
             type: "DELETE",
+            success: function() {
+                $("#" + util["type"] + "-" + util["id"]).fadeOut(200, function() { this.remove(); });
+            },
             error: function(response) {
                 console.log("Creatoin of util failed with response: " + JSON.stringify(response));
             }
