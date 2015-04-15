30fa7300145cf6aa72b465c681a4d30e6a56c1d0
batedurgonnadie <batedurgonnadie@yahoo.com>
2015-04-14 20:39:42 -0700

    Add landing for mask

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def faq
   end
+
+  def mask
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
 
   get   'twitch/dashboard',           to: 'dashboard#dashboard',    as: :dashboard
 
+
+  get   'github/mask',                to: 'static_pages#mask',      as: :mask
+
   # API Stuff
   namespace :api do
     resources :users, only: [] do
