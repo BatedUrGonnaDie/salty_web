6a0be20b18157d2f953f8380e0b96307af68dff0
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-04-16 15:37:59 -0700

    Update quote creation
    Now moves the new quote into the form.  Does not submit the new quotes
    though.  In order to get the delete button or edit it you will need to
    refresh.

--- app/assets/javascripts/textutil_create.js
+++ app/assets/javascripts/textutil_create.js
@@ -5,6 +5,16 @@ $(function () {
                 url: "/api/users/" + util["user"] + "/" + util["type"],
                 type: "POST",
                 data: "&text=" + encodeURIComponent(util["text"]) + "&reviewed=1",
+                success: function(response) {
+                    var type_single = util["type"].substring(0, util["type"].length - 1);
+                    $("#new_" + util["type"] + "_input").val("");
+                    var input_stuff = "<div id='" + util["type"] + "-" + response[type_single]["id"] + "'class='pure-u-1-2'>";
+                    input_stuff += "<div class='textutil'>"
+                    input_stuff += "<input type='text' value='" + util["text"] + "' />";
+                    input_stuff += "</div></div>"
+                    $("#" + util["type"] + "-form").append(input_stuff);
+                    console.log(input_stuff);
+                },
                 error: function(response) {
                     console.log("Creatoin of util failed with response: " + JSON.stringify(response));
                 }
