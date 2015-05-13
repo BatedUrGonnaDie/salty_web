976b0a305843e9d0969e9a50868712d5410a9321
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-05-13 01:57:00 -0700

    Revert "Update application name"
    This reverts commit 788b4257557036a0c334badf94214d28fc44f85a.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SaltyWeb::Application.config.session_store :cookie_store, key: '_salty_web_session'
+Rails.application.config.session_store :cookie_store, key: '_salty_web_v2_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SaltyWeb::Application.routes.draw do
+Rails.application.routes.draw do
   root  'static_pages#home'
 
   get   '/contact',                   to: 'static_pages#contact',   as: :contact
