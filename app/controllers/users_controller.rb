c838658ead8b87a0505bda1676c46866eac4692d
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-05-20 04:58:30 -0700

    Update to allow development pushing to bot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,10 +77,12 @@ class UsersController < ApplicationController
     end
 
     def send_update
-      unless !Rails.env.development?
-        return true
+      if Rails.env.development?
+        ip = "localhost"
+      else
+        ip = ENV["salty_ip_address"]
       end
-      sock = TCPSocket.new(ENV["salty_ip_address"], 6666)
+      sock = TCPSocket.new(ip, 6666)
       sock.write(ENV["salty_web_secret"])
       sock.write JSON.generate({user_id: @user.id})
       sock.close()
