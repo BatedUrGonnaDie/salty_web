fda6acc8c41e3e040298ec8c033713329532f099
batedurgonnadie <batedurgonnadie@yahoo.com>
2015-08-06 15:59:05 -0700

    Works fine with songs, errors on blank primary

--- app/controllers/api/users/songs_controller.rb
+++ app/controllers/api/users/songs_controller.rb
@@ -1,13 +1,13 @@
 class Api::Users::SongsController < Api::ApplicationController
   before_action :set_user
+  before_action :check_for_key, only: [:create]
+  before_action :check_for_primary, only: [:create]
 
   def index
     render status: 200, json: {status: 200, song: "#{get_song}"}
   end
 
   def create
-    check_for_key
-    check_for_primary
     @settings[:osu_current_song] = params[:primary]
     if @settings.save
       render status: 200, json: {status: 200, song: @settings[:osu_current_song]}
@@ -38,12 +38,10 @@ class Api::Users::SongsController < Api::ApplicationController
     def check_for_primary
       if params[:primary].present?
         if params[:primary].empty?
-          render status: 200, json: {status: 200}
-          return
+          head 200 and return
         end
       else
-        render status: 200, json: {status: 200}
-        return
+        head 200 and return
       end
     end
 end
