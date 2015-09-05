bb2d7cf0f55a10e8271dd70dbce9e82c7c03b8fd
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-09-05 00:43:23 -0700

    Lots of changes
    Adds progress bar, switches to 3 width columns, remove outliers from
    mean, allow comparing to median of history, stop from flashing and
    disappearing, fix for skipped splits for first graph

--- app/assets/javascripts/generate_graph.js
+++ app/assets/javascripts/generate_graph.js
@@ -2,56 +2,72 @@ $(function () {
     window.generate_graph = function() {
         run_id = $("#splits-run-id").val();
         if (run_id != "") {
+            update_progrss("0", "Fetching run...");
             $("#chart").fadeOut(200, function() {
                 $(this).empty().show();
                 if (sessionStorage.getItem(run_id)) {
-                    response = JSON.parse(sessionStorage.getItem(run_id));
-                    add_basic(response.run);
-                    add_splits(response.run.splits, "PB vs. Golds", false);
-                    if (response.run.program == "livesplit") {
-                        add_splits(response.run.splits, "History Mean vs. Golds", true);
-                    };
+                    update_progrss(50, "Parsing splits...")
+                    graph_init(run_id);
+                } else {
+                    $.ajax({
+                        url: "https://splits.io/api/v3/runs/" + run_id,
+                        type: "GET",
+                        crossDomain: true,
+                        success: function(response) {
+                            sessionStorage.setItem(run_id, JSON.stringify(response));
+                            update_progrss(50, "Parsing splits...")
+                            graph_init(run_id);
+                        },
+                        error: function(response) {
+                            update_progrss(100, "Error retrieving splits");
+                            console.log(response);
+                        }
+                    });
                 };
             });
-            if (!sessionStorage.getItem(run_id)) {
-                $.ajax({
-                    url: "https://splits.io/api/v3/runs/" + run_id,
-                    type: "GET",
-                    crossDomain: true,
-                    success: function(response) {
-                        sessionStorage.setItem(run_id, JSON.stringify(response));
-                        add_basic(response.run);
-                        add_splits(response.run.splits, "PB vs. Golds", false);
-                        if (response.run.program == "livesplit") {
-                            add_splits(response.run.splits, "History Mean vs. Golds", true);
-                        };
-                    },
-                    error: function(response) {
-                        console.log(response);
-                    }
-                })
-            };
+
         } else {
             // red required
         };
     };
 });
 
+var update_progrss = function(percentage, new_text) {
+    var bar = $("#graph-progress");
+    bar.attr("style", "mid-width: 2em; width: " + percentage + "%;");
+    bar.attr("aria-valuenow", percentage + "%");
+    if (new_text) {
+        bar.text(new_text);
+    };
+};
+
+var graph_init = function(run_id) {
+    response = JSON.parse(sessionStorage.getItem(run_id));
+    add_basic(response.run);
+    add_splits(response.run.splits, "PB vs. Golds", false);
+    if (response.run.program.toLowerCase() === "livesplit") {
+        add_splits(response.run.splits, "Split History " + $("#history-compare-type").val() + " vs. Golds", true);
+    };
+};
+
 var add_basic = function(run) {
 
-}
+};
 
 var add_splits = function(splits, graph_title, with_history) {
     var splits_small = []
-    splits.forEach(function(d) {
-        if (with_history) {
+    if (with_history) {
+        splits.forEach(function(d) {
             var split = validate_history(d);
-        } else {
-            var split = validate_split(d);
-        }
-        if (split) { splits_small.push(split)};
-    });
+            if (split) { splits_small.push(split); };
+            update_progrss(50 + (30 / splits.length));
+        });
+    } else {
+        splits_small = condense_splits(splits);
+    };
     if (splits_small.length === 0) { return; };
+    console.log(splits_small);
+    update_progrss(80, "Creating graphs...");
 
     var margin = {top: 30, right: 30, bottom: 120, left: 60},
         width = $("#chart").width() - margin.left - margin.right,
@@ -96,6 +112,8 @@ var add_splits = function(splits, graph_title, with_history) {
        .attr("dx", "-.8em")
        .attr("dy", ".15em")
        .attr("transform", "rotate(-45)");
+    update_progrss(85);
+
     svg.append("g")
        .attr("class", "y axis")
        .call(yAxis)
@@ -106,6 +124,8 @@ var add_splits = function(splits, graph_title, with_history) {
        .attr("dy", "1em")
        .style("text-anchor", "middle")
        .text("Possible Time Save");
+    update_progrss(90);
+
     svg.append("text")
        .attr("x", width / 2)
        .attr("y", 0 - margin.top / 2)
@@ -113,6 +133,7 @@ var add_splits = function(splits, graph_title, with_history) {
        .style("font-size", "16px")
        .style("text-decoration", "underline")
        .text(graph_title);
+    update_progrss(95);
 
     svg.selectAll("bar")
        .data(splits_small)
@@ -127,24 +148,51 @@ var add_splits = function(splits, graph_title, with_history) {
        .append("title")
        .attr("class", "bar-text")
        .text(function(d) { return d.value + " seconds"; });
+       update_progrss(100, "Ready");
 };
 
-var validate_split = function(split) {
-    if (split.gold || split.skipped) {
-        return false;
-    } else if (split.duration - split.best.duration < Number($("#min-bar-time").val())) {
-        return false;
-    } else {
-        return {name: split.name, value: Math.round((split.duration - split.best.duration) * 100) / 100}
+var condense_splits = function(splits) {
+    var reduced_splits = []
+    for (var i = 0, length = splits.length; i < length; ++i) {
+        if (splits[i].skipped) {
+            if (i + 1 < length) {
+                splits[i+1].best += splits[i].best;
+            } else {
+                splits.pop();
+            };
+            update_progrss(50 + (15 / splits.length));
+        };
     };
-};
+    for (var i = 0, length = splits.length; i < length; ++i) {
+        var cmp_time = splits[i].duration - splits[i].best.duration;
+        if (cmp_time > Number($("#min-bar-time").val())) {
+            reduced_splits.push({name: splits[i].name, value: Math.round((cmp_time) * 100) / 100});
+        };
+        update_progrss(50 + (15 / splits.length));
+    };
+    return reduced_splits;
+}
 
 var validate_history = function(split) {
     if (split.history === null || split.history.length === 0) { return false; };
-    split.history = split.history.filter(function(d) { if (d == 0) { return false; } return true; });
-    var mean = d3.mean(split.history);
-    if (mean - split.best.duration < Number($("#min-bar-time").val())) { return false; };
-    return {name: split.name, value: Math.round((mean - split.best.duration) * 100) / 100};
+
+    var cmp_type = $("#history-compare-type").val();
+    if (cmp_type === "Mean/Average") {
+        var min_cap = split.best.duration * .2,
+            max_cap = split.best.duration * 1.8;
+        split.history = split.history.filter(function(d) {
+            if (d == 0) { return false; }
+            else if (d > max_cap || d < min_cap) { return false; };
+            return true;
+        });
+        var mean = d3.mean(split.history);
+        if (mean - split.best.duration < Number($("#min-bar-time").val())) { return false; };
+        return {name: split.name, value: Math.round((mean - split.best.duration) * 100) / 100};
+    } else if (cmp_type === "Median") {
+        var median = d3.median(split.history);
+        if (median - split.best.duration < Number($("#min-bar-time").val())) { return false; };
+        return {name: split.name, value: Math.round((median - split.best.duration) * 100) / 100};
+    };
 };
 
 var export_to_png = function() {
