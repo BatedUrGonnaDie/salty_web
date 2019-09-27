e4e4e3233c8bf9ab4e01d9ef4ffdb966ba460340
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2019-09-26 21:07:33 -0700

    not sure what this is anymore tbh

--- app/controllers/api/users/songs_controller.rb
+++ app/controllers/api/users/songs_controller.rb
@@ -5,7 +5,7 @@ class Api::Users::SongsController < Api::ApplicationController
   before_action :check_for_primary, only: [:create]
 
   def index
-    render status: 200, json: {status: 200, song: "#{get_song}"}
+    render status: 200, json: {status: 200, song: "#{@user.settings[:osu_current_song]}"}
   end
 
   def create
@@ -26,31 +26,31 @@ class Api::Users::SongsController < Api::ApplicationController
   end
 
   private
-    def get_song
-      @user.settings[:osu_current_song]
-    end
 
-    def check_for_key
-      if params[:key].present?
-        if @user.settings[:osu_song_key] != params[:key] && @user.settings[:osu_song_key] != nil
-          render status: 401, json: {status: 401, error: "This endpoint requires a valid key."}
-          return
-        else
-          @settings = @user.settings
-        end
+  def check_for_key
+    if params[:key].present?
+      if @user.settings[:osu_song_key] != params[:key] && @user.settings[:osu_song_key] != nil
+        render status: 401, json: {status: 401, error: "This endpoint requires a valid key."}
+        return
       else
-        render status: 400, json: {status: 400, error: "This endpoint requires the \"key\" parameter."}
+        @settings = @user.settings
+      end
+    else
+      render status: 400, json: {status: 400, error: "This endpoint requires the \"key\" parameter."}
+      return
+    end
+  end
+
+  def check_for_primary
+    binding.pry
+    if params[:primary].present?
+      if params[:primary].empty?
+        render status: 200, json: {status: 200}
         return
       end
+    else
+      render status: 200, json: {status: 200}
+      return
     end
-
-    def check_for_primary
-      if params[:primary].present?
-        if params[:primary].empty?
-          render status: 200, json: {status: 200} and return
-        end
-      else
-        render status: 200, json: {status: 200} and return
-      end
-    end
+  end
 end
